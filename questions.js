30ea89beb9c8ae37244c24679b58435158e413c8
JoyceYeung1 <9556764-JoyceYeung1@users.noreply.replit.com>
2023-12-16 06:43:32 +0000

    Initial commit

---
+++ questions.js
@@ -0,0 +1,233 @@
+
+let animals = [
+  {
+    q:"What is this animal?",
+    img:"pictures/animals/dog.jpg",
+    a:["Dog","Cat","Wolf","Lion"],
+    score:100,
+    boo:0
+  },
+  
+  {
+    q:"What is this animal?",
+    img:"pictures/animals/hamster.jpg",
+    a:["Hamster","Mice","Guinea Pig","Chinchilla"],
+    score:200,
+    boo:0
+  },
+  {
+    q:"What is this animal?",
+    img:"pictures/animals/kdragon.jpg",
+    a:["Kamodo Dragon","Dragon","Lizard","Aligator"],
+    score:300,
+    boo:0
+  },
+  {
+    q:"What is this animal?",
+    img:"pictures/animals/rpanda.jpg",
+    a:["Red Panda","Panda","Bear","Fox"],
+    score:400,
+    boo:0
+    
+  },
+  {
+    q:"What is this animal?",
+    img:"pictures/animals/quokkas.jpg",
+    a:["Quokkas","Monkey","Bat","Kangaroo"],
+    score:500,
+    boo:0
+  }
+  
+]
+
+let movies = [
+  {
+    q:"What is show is this?",
+    img:"pictures/movies/frozen.jpg",
+    a:["Frozen","Ice Queen","Snowman","Trolls"],
+    score:100,
+    boo:0
+  },
+  {
+    q:"What is show is this?",
+    img:"pictures/movies/teletubbies.jpg",
+    a:["Teletubbies","Telebubbies","Baby Sun","Telebabies"],
+    score:200,
+    boo:0
+  },
+  {
+    q:"What is show is this?",
+    img:"pictures/movies/rio.jpg",
+    a:["Rio","Birds","Blue Birds","Madegascar"],
+    score:300,
+    boo:0
+    
+  },
+  {
+    q:"What is show is this?",
+    img:"pictures/movies/mhouse.jpg",
+    a:["Monster House","Haunted House","Monster Univerity","Halloween"],
+    score:400,
+    boo:0
+    
+  },
+  {
+    q:"What is show is this?",
+    img:"pictures/movies/barnyard.jpg",
+    a:["Back at the Barnyard","Farm","Back at the Farm","Farmer"],
+    score:500,
+    boo:0
+    
+    
+  }
+  
+]
+
+let riddles = [
+  {
+    q:"What has four fingers and a thumb but isn't alive?",
+    a:["A glove","Paws","Chair","People"],
+    score:100
+    
+    
+  },
+  {
+    q:"If you have me, you will want to share me. If you share me, you will no longer have me. What am I?",
+    a:["Secret","Food","Toys","Phone"],
+    score:200
+    
+  },
+  {
+    q:"What two words, added together, contain the most letters?",
+    a:["Post Office","The Alphabet","An Essay","A Book"],
+    score:300
+  },
+  {
+    q:"What has a head and a tail, but no body?",
+    a:["Coin","Snake","Eel","Boat"],
+    score:400
+  },
+  {
+    q:"I follow you all the time and copy your every move, but you can’t touch or catch me. What am I?",
+    a:["Shadow","Wind","Sun","Cloud"],
+    score:500
+  }
+  
+]
+
+let Candies = [
+  {
+    q:"What candy is this?",
+    img:"pictures/candies/skittles.jpg",
+    a:["Skittles","m&m","Jolly Ranchers","Swedish Fish"],
+    score:100,
+    boo:0
+    
+    
+  },
+  {
+    q:"What candy is this?",
+    img:"pictures/candies/rpop.jpg",
+    a:["Ring Pop","Baby bottles","Push Pop","Sour Punch"],
+    score:200,
+    boo:0
+    
+  },
+  {
+    q:"What candy is this?",
+    img:"pictures/candies/peeps.jpg",
+    a:["Peeps","Gummy","Life Savers","Bottle Caps"],
+    score:300,
+    boo:0
+  },
+  {
+    q:"What candy is this?",
+    img:"pictures/candies/sstrips.jpg",
+    a:["Sour strips","Air heads","Twizziers","Sour Patch"],
+    score:400,
+    boo:0
+  },
+  {
+    q:"What candy is this?",
+    img:"pictures/candies/pez.jpg",
+    a:["Pez","Sweet Tarts","Smarties","Rolo"],
+    score:500,
+    boo:0
+  }
+  
+]
+
+let Disney = [
+  {
+    q:"Which disney movies is this?",
+    quote:"disney/beast.mp3",
+    a:["Beauty and the Beast","Tangled","Mulan","Frozen"],
+    score:100,
+    boo:2
+    
+    
+  },
+  {
+    q:"Which disney movies is this?",
+    quote:"disney/ariel.mp3",
+    a:["The Little Mermaid","Snow White","Sleeping Beauty","Peter Pan"],
+    score:200,
+    boo:2
+    
+  },
+  {
+    q:"Which disney movies is this?",
+    quote:"disney/nemo.mp3",
+    a:["Finding Nemo","Sing","Luca","Turning Red"],
+    score:300,
+    boo:2
+  },
+  {
+    q:"Which disney movies is this?",
+    quote:"disney/cinderella.mp3",
+    a:["Cinderella","Aladdin","The Princess and the Frog","Mary Poppins"],
+    score:400,
+    boo:2
+  },
+  {
+    q:"Which disney movies is this?",
+    quote:"disney/ralph.mp3",
+    a:["Wreck it Ralph","The Lion King","Dalmations 101","Moana"],
+    score:500,
+    boo:2
+  }
+  
+]
+
+let Fun_Facts = [
+  {
+    q:"How many bones does a shark have?",
+    a:["0","100","50","500"],
+    score:100
+    
+    
+  },
+  {
+    q:"How many noes does a slug have?",
+    a:["4","1","8","10"],
+    score:200
+    
+  },
+  {
+    q:"What color is the sunset on Mars?",
+    a:["Blue","Red","Pink","Yellow"],
+    score:300
+  },
+  {
+    q:"What is the largest organ on your body?",
+    a:["Skin","Liver","Heart","Brain"],
+    score:400
+  },
+  {
+    q:"How many brains does a starfish have?",
+    a:["0","5","1","3"],
+    score:500
+  }
+  
+]
+
---
+++ script.js
@@ -0,0 +1,207 @@
+//https://www.w3schools.com/css/css3_animations.asp
+let cards;
+let category = ["Animals", "Movies","Riddles","Candies","Disney","Fun Fact"];
+let categories = [animals,movies,riddles,Candies,Disney,Fun_Facts];
+let build=""; 
+let build2="";
+let scores = 0;
+let someone_has_pressed = false;
+let key;
+let timer = 10;
+let play = false;
+let num = 0;
+let players = [
+  {"player":1,"score":0,"name":""},
+  {"player":2,"score":0,"name":""},
+  {"player":3,"score":0,"name":""},
+  {"player":4,"score":0,"name":""}
+]
+let count;
+let total = 0;
+let winner;
+
+let sound = new Howl({
+  src: ['correct.mp3']
+});
+
+let incorrect = new Howl({
+  src: ['wrong.mp3']
+});
+
+let song = new Howl({
+  src: ['jeopardy.mp3'],
+  loop:true,
+  volume: 0.5
+});
+
+let ding = new Howl({
+  src: ['ding.mp3']
+});
+
+function init(){
+  song.play();
+  count = localStorage.getItem("count");
+  let op = document.getElementById("output");
+  const card = document.getElementById("card");
+  document.getElementById("output2").innerHTML = updateScoreBoard();
+  window.addEventListener("keypress",function(e){
+    if( !someone_has_pressed  && play){
+      if(e.key == "a"){
+        document.getElementById("h1").classList.toggle("glow2");
+        console.log("Player 1");
+        ding.play();
+        someone_has_pressed = true;
+      }
+      if(e.key == "p"){
+        console.log("Player 2");
+        ding.play();
+        document.getElementById("h2").classList.toggle("glow2");
+        someone_has_pressed = true;
+      }
+      if(e.key == "n"){
+        console.log("Player 3");
+        ding.play();
+        document.getElementById("h3").classList.toggle("glow2");
+        someone_has_pressed = true;
+      }
+      if(e.key == "t"){
+        console.log("Player 4");
+        ding.play();
+        document.getElementById("h4").classList.toggle("glow2");
+        someone_has_pressed = true;
+      }
+      key = e.key;
+    }
+  })
+
+  
+  for(let i=0; i <=5; i++){
+    build += `<div class="category">`;
+    build +=    `<h3>${category[i]}</h3>`;
+    build += `</div>`;
+  }
+point = 0;
+for(let i=0; i <=4 ; i++){
+  point += 100;
+
+  for(let j=0; j <=5 ; j++){
+    let order = [0,1,2,3];
+    order.sort(() => Math.random() - 0.5);
+    let x = order[0];
+    let y = order[1];
+    let z = order[2];
+    let p = order[3];
+    build +=`<div class="card" id="card${j}_${i}">`;
+    build +=    `<div class="front">`
+    build +=      `<h3>${point}</h3>`;
+    build +=    `</div>`
+    build +=    `<div class="back">`
+    build +=      `<h4>${categories[j][i].q}</h4>`;
+    if(categories[j][i].boo==0){
+      build +=      `<img src=${categories[j][i].img} height= "100" width= "100">`;
+    }
+
+    if(categories[j][i].boo == 2){
+      build +=      `<audio controls>`;
+      build +=        `<source src=${categories[j][i].quote} type="audio/mpeg">`;
+      build +=      `</audio>`;
+    }
+    build +=      `<br>`;
+    build +=      `<input type="radio"  name="q${j}_${i}" value="${categories[j][i]["a"][x]}">
+         <label for="ca" >${categories[j][i]["a"][x]}</label><br>`
+    build +=      `<input type="radio"  name="q${j}_${i}" value="${categories[j][i]["a"][y]}">
+         <label for="ca" >${categories[j][i]["a"][y]}</label><br>`
+    build +=      `<input type="radio"  name="q${j}_${i}" value="${categories[j][i]["a"][z]}">
+         <label for="ca" >${categories[j][i]["a"][z]}</label><br>`
+    build +=      `<input type="radio"  name="q${j}_${i}" value="${categories[j][i]["a"][p]}">
+         <label for="ca" >${categories[j][i]["a"][p]}</label><br>`
+    build += `<input type="button" onclick="checkAnswer(${j},${i})" value="Check">`
+    build +=    `</div>`
+    build +=`</div>`;
+  }
+}
+  op.innerHTML=build;
+  cards = document.querySelectorAll(".card .front");
+  for(let i = 0; i < cards.length; i++){
+    cards[i].addEventListener("click", function(){
+      if(!this.parentElement.classList.contains("flipCard") && someone_has_pressed){
+        this.parentElement.classList.toggle("flipCard");
+      } 
+    });
+  }
+updateTime();
+}
+
+
+function checkAnswer(j,i){
+  let user_answer = document.querySelector(`input[name="q${j}_${i}"]:checked`).value;
+  if(categories[j][i]["a"][0] == user_answer){
+    scores = categories[j][i]["score"];
+    if(key == "a"){
+      players[0].score += scores;
+    }else if(key == "p"){
+      players[1].score += scores;
+    }else if(key == "n"){
+      players[2].score += scores;
+    }else if(key == "t"){
+      players[3].score += scores;
+    }
+    sound.play();
+  }else{
+    incorrect.play();
+    document.getElementById(`card${j}_${i}`).classList.toggle("flipCard");
+  }
+  document.getElementById("output2").innerHTML = updateScoreBoard();
+  total = players[0].score + players[1].score + players[2].score + players[3].score;
+  if(total == 9000){
+    if((players[0].score > players[1].score) && (players[0].score > players[2].score) && (players[0].score > players[3].score)){
+      localStorage.setItem("winner", "Player 1 Wins!!!");
+    }else if((players[1].score > players[0].score) && (players[1].score > players[2].score) && (players[1].score > players[3].score)){
+      localStorage.setItem("winner", "Player 2 Wins!!!");
+    }else if((players[2].score > players[0].score) && (players[2].score > players[1].score) && (players[2].score > players[3].score)){
+      localStorage.setItem("winner", "Player 3 Wins!!!");
+    }else if((players[3].score > players[0].score) && (players[3].score > players[1].score) && (players[3].score > players[2].score)){
+      localStorage.setItem("winner", "Player 4 Wins!!!");
+    }
+    window.location.href = "end.html";
+  }
+  someone_has_pressed = false;
+  timer = 10;
+  play = false;
+  key = "";
+}
+function setPlayers(amt){
+  localStorage.setItem("count", amt);
+  window.location.href = "play.html";
+}
+
+function updateScoreBoard(){
+  build2 = "";
+  for(let t=0; t<count; t++){
+    build2 += `<h3 id="h${t+1}">Player ${players[t]["player"]}:${players[t]["score"]}</h3>`;
+  }
+  return build2;
+}
+
+function updateTime(){
+  if(timer > 0){
+    timer--;
+  }else{
+    play = true;
+  }
+
+  if(timer > 6){
+    document.getElementById("output3").style.color = "#f23a50";
+  }else if(timer > 3){
+    document.getElementById("output3").style.color = "#fafa69";
+  }else{
+    document.getElementById("output3").style.color = "#bdfab9";
+  }
+  
+  document.getElementById("output3").innerHTML = timer;
+  setTimeout(updateTime,1000);
+  
+}
+
+
+
